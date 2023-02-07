3608b3b8ab7494c03df8b23ac5ab4b6fc33e3c70
Dmitry Homskiy <workfids23@gmail.com>
2023-02-07 21:39:21 +0300

    fix header

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 		<div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 ">
 			<div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
 				<div className="flex flex-col justify-center h-full">
-					<h2 className="text-4xl sm:text-7xl font-bold text-white">I'm Frontend Developer</h2>
+					<h2 className="pt-8 text-4xl sm:text-7xl font-bold text-white">I'm Frontend Developer</h2>
 					<p className="text-gray-300 py-4 max-w-md">
 						I have more than a year of experience creating and developing front-end. Currently I love working on web applications using technologies such as: React, JavaScript, Tailwind,
 						SCSS and others.
